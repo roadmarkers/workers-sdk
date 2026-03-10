6f5ac8a1f0d7de6d263793012095c88473420ef7
Edmund Hung <edmund@cloudflare.com>
2026-03-10 18:03:27 +0000

    fix(create-cloudflare): astro v6 workers setup

--- packages/create-cloudflare/templates/astro/workers/c3.ts
+++ packages/create-cloudflare/templates/astro/workers/c3.ts
@@ -1,11 +1,9 @@
-import { logRaw, updateStatus } from "@cloudflare/cli";
-import { blue, brandColor, dim } from "@cloudflare/cli/colors";
+import { logRaw } from "@cloudflare/cli";
+import { brandColor, dim } from "@cloudflare/cli/colors";
 import { runFrameworkGenerator } from "frameworks/index";
-import { transformFile } from "helpers/codemod";
 import { runCommand } from "helpers/command";
 import { usesTypescript } from "helpers/files";
 import { detectPackageManager } from "helpers/packageManagers";
-import * as recast from "recast";
 import type { TemplateConfig } from "../../../src/templates";
 import type { C3Context, PackageJson } from "types";
 
@@ -16,10 +14,6 @@ const generate = async (ctx: C3Context) => {
 	// The adapter is added in the `configure` step instead
 	await runFrameworkGenerator(ctx, [
 		ctx.project.name,
-		// Use v5 "basics" template:
-		"--template",
-		// https://github.com/withastro/astro/tree/64533807745d8f2a298e0ad83757e7f05b10c7ba/examples/basics
-		"withastro/astro/examples/basics#64533807745d8f2a298e0ad83757e7f05b10c7ba",
 		// c3 will later install the dependencies
 		"--no-install",
 		// c3 will later ask users if they want to use git
@@ -36,42 +30,6 @@ const configure = async () => {
 		doneText: `${brandColor("installed")} ${dim(
 			`via \`${npx} astro add cloudflare\``,
 		)}`,
-	});
-
-	// Update Astro config to enable platformProxy and imageService
-	const filePath = "astro.config.mjs";
-
-	updateStatus(`Updating configuration in ${blue(filePath)}`);
-
-	transformFile(filePath, {
-		visitCallExpression: function (n) {
-			const callee = n.node.callee as recast.types.namedTypes.Identifier;
-			if (callee.name !== "cloudflare") {
-				return this.traverse(n);
-			}
-
-			const b = recast.types.builders;
-			n.node.arguments = [
-				b.objectExpression([
-					// platformProxy: {
-					//   enabled: true,
-					// },
-					b.objectProperty(
-						b.identifier("platformProxy"),
-						b.objectExpression([
-							b.objectProperty(b.identifier("enabled"), b.booleanLiteral(true)),
-						]),
-					),
-					// imageService: "cloudflare",
-					b.objectProperty(
-						b.identifier("imageService"),
-						b.stringLiteral("cloudflare"),
-					),
-				]),
-			];
-
-			return false;
-		},
 	});
 };
 
@@ -107,7 +65,7 @@ const config: TemplateConfig = {
 	transformPackageJson: async (pkgJson: PackageJson, ctx: C3Context) => ({
 		scripts: {
 			deploy: `astro build && wrangler deploy`,
-			preview: `astro build && wrangler dev`,
+			preview: `astro build && astro preview`,
 			...(usesTypescript(ctx) && { "cf-typegen": `wrangler types` }),
 		},
 	}),
